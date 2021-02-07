3b11f3193fc5e0ad0c0d0264e2ef0037e63b5d55
opoink <37372895+opoink@users.noreply.github.com>
2021-02-07 15:06:27 +0800

    updates for configuration

--- App/node/webpack.config.js
+++ App/node/webpack.config.js
@@ -1,43 +1,62 @@
-var path = require('path');
-var webpack = require('webpack');
-var MiniCssExtractPlugin = require('mini-css-extract-plugin');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+const ROOT = path.dirname(path.dirname(__dirname));
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const WebpackBeforeBuildPlugin = require('before-build-webpack');
+const exec = require('child_process').exec;
+const appConfig = require('./src/app.config.json');
 
-module.exports = {
-    mode: 'development',
+var today = new Date();
+var insec = parseInt(today.valueOf() / 1000); 
+
+var plugins = [];
+plugins.push(new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery'
+}));
+plugins.push(new HtmlWebpackPlugin({
+    inject: 'body',
+    template: './src/index.html'
+}));
+plugins.push(new MiniCssExtractPlugin({
+    filename: appConfig.prod ? insec + 'style.css' : 'style.css' 
+}));
+plugins.push(new CleanWebpackPlugin({
+    cleanAfterEveryBuildPatterns: [appConfig.ouputPath]
+}));
+plugins.push(new WebpackBeforeBuildPlugin(function(stats, callback) {
+    exec('php -v', (error, stdOut, stdErr) => {
+        callback();
+    }); 
+}));
+
+
+var config = {
+    mode: appConfig.prod ? 'production' : 'development',
     entry: './src/app.ts',
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        // publicPath: '/dist'
+        path: path.resolve(__dirname, appConfig.ouputPath),
+        filename: appConfig.prod ? insec + appConfig.outputFileName : appConfig.outputFileName
     },
     module: {
         rules: [
-            { 
-                test: /\.ts$/, 
-                use: 'awesome-typescript-loader' 
-            },
-            { 
-                test: /\.js$/, 
-                use: 'babel-loader' 
-            },
+            { test: /\.ts$/, use: 'awesome-typescript-loader' },
+            { test: /\.js$/, use: 'babel-loader' },
             { 
                 test: /\.scss$/, 
                 use: [
                     {
                         loader: MiniCssExtractPlugin.loader,
                         options: {
-                            publicPath: '/dist',
+                            publicPath: '/' + appConfig.publicPath,
                         }
                     },
                     'css-loader',
                 ] 
             },
-            { 
-                test: /\.html$/, 
-                use: 'html-loader' 
-            },
+            { test: /\.html$/, use: 'html-loader' },
             {
                 test: /\.(gif|png|jpe?g|svg)$/i,
                 use: [
@@ -53,20 +72,8 @@ module.exports = {
             }
         ]
     },
-    plugins: [
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery'
-        }),
-        new HtmlWebpackPlugin({
-            inject: 'body',
-            template: './src/index.html'
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'style.css'
-        }),
-        new CleanWebpackPlugin({
-            cleanAfterEveryBuildPatterns: ['dist']
-        })
-    ]
+    plugins: plugins
 }
+
+
+module.exports = config;
