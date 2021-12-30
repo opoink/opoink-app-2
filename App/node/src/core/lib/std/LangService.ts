457acd3f61dbcaf215b922b1ecc44e74815c22e4
opoink <ronromeperez@yahoo.com>
2021-12-30 16:48:01 +0800

    updates for the lang service, and add comment sample

--- App/node/src/core/lib/std/LangService.ts
+++ App/node/src/core/lib/std/LangService.ts
@@ -2,19 +2,39 @@ import * as languages from "./../../../var/languages.json";
 
 export class LangService {
 
-	getLang(key:string, language, values:any = null){
+	/**
+	 * 
+	 * @param key string from your JSON file sample "Hello world": "Hello World!" where "Hello world" is the key
+	 * @param language string the name of your json file sample en.json where "en" is the language
+	 * @param values array of object
+	 * sample string, Welcome back {{firstname}}! outpout Welcome back John!
+	 * interpollation can be used in your service ts
+	 * 
+	 * 	let msg = this.langService.getLang("Welcome back, {{firstname}} {{lastname}}!", "en", [
+	 *		{key: "firstname",value: user.firstname},
+	 *		{key: "lastname",value: user.lastname}
+	 *	]);
+	 * console.log(msg);
+	 * 
+	 * @returns string
+	 */
+	getLang(key:string, language:string, values:any = null){
 		if(typeof languages[language] != 'undefined'){
-			if(values){
-				let l:string = languages[language][key];
-				values.forEach(value => {
-					l = l.replace("{{" + value.key + "}}", value.value);
-				});
-				return l;
+			if(typeof languages[language][key] != 'undefined'){
+				if(values){
+					let l:string = languages[language][key];
+					values.forEach(value => {
+						l = l.replace("{{" + value.key + "}}", value.value);
+					});
+					return l;
+				} else {
+					return languages[language][key];
+				}
 			} else {
-				return languages[language][key];
+				return ' undefined text: ' + key ;
 			}
 		} else {
-			return key + ' undefined language text';
+			return ' undefined language: ' + language + ' - ' + key;
 		}
 	}
 }
