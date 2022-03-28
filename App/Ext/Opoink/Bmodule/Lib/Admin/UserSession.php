e1a4c2151ea40d8c5921dadca2d4f55bbceb521b
opoink <ronromeperez@yahoo.com>
2022-03-28 04:19:51 +0000

    start of localization

--- App/Ext/Opoink/Bmodule/Lib/Admin/UserSession.php
+++ App/Ext/Opoink/Bmodule/Lib/Admin/UserSession.php
@@ -8,6 +8,15 @@ namespace Opoink\Bmodule\Lib\Admin;
 
 class UserSession extends \Of\Session\Session {
 
+
+	/**
+	 * if the user is logged in this variable will be
+	 * \Opoink\Bmodule\Entity\AdminsEntity 
+	 * with the data of the current admin user
+	 * if not the this will be null;
+	 */
+	protected $adminUser = null;
+
 	/**
 	 * \Opoink\Bmodule\Entity\AdminsEntity
 	 */
@@ -29,15 +38,29 @@ class UserSession extends \Of\Session\Session {
 	/**
 	 * return \Opoink\Bmodule\Entity\Admins if the user is loged in
 	 * else return null
+	 * 
+	 * we will store the user data on first load so that the other 
+	 * blocks or class that will call this function 
+	 * will not fetch the same data again and again
+	 */
+	public function getAdminUser(){
+		if(!$this->adminUser) {
+			$admin_user = $this->getData('admin_user');
+			if($admin_user){
+				$this->adminUser = $this->_adminsEntity->getByColumn([
+					'admins_id' => $admin_user['admins_id']
+				]);
+			}
+		}
+		return $this->adminUser;
+	}
+
+	/**
+	 * return \Opoink\Bmodule\Entity\Admins if the user is loged in
+	 * else return null
 	 */
 	public function isLoggedIn(){
-		$admin_user = $this->getData('admin_user');
-
-		if($admin_user){
-			return $this->_adminsEntity->getByColumn([
-				'admins_id' => $admin_user['admins_id']
-			]);
-		}
+		return $this->getAdminUser();
 	}
 
 	/**
